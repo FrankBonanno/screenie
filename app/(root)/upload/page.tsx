1a96bca5da08a1006815ece0646db876163cc077
FrankBonanno <fb0023@mix.wvu.edu>
2025-05-20 04:46:46 -0400

    upload page

---
+++ app/(root)/upload/page.tsx
@@ -0,0 +1,116 @@
+'use client';
+import FileInput from '@/components/FileInput';
+import FormField from '@/components/FormField';
+import { MAX_THUMBNAIL_SIZE, MAX_VIDEO_SIZE } from '@/constants';
+import { useFileInput } from '@/lib/hooks/useFileInput';
+import { ChangeEvent, FormEvent, useState } from 'react';
+
+const UploadPage = () => {
+	const [formData, setFormData] = useState({
+		title: '',
+		description: '',
+		visibility: 'public',
+	});
+	const [error, setError] = useState('');
+	const [isSubmitting, setIsSubmitting] = useState(false);
+
+	const video = useFileInput(MAX_VIDEO_SIZE);
+
+	const thumbnail = useFileInput(MAX_THUMBNAIL_SIZE);
+
+	const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = e.target;
+		setFormData((prevState) => ({ ...prevState, [name]: value }));
+	};
+
+	const handleSubmit = async (e: FormEvent) => {
+		e.preventDefault();
+
+		setIsSubmitting(true);
+
+		try {
+			if (!video.file || !thumbnail.file) {
+				setError('Please upload video and thumbnail!');
+				return;
+			}
+
+			if (!formData.title || !formData.description) {
+				setError('Please fill in all details!');
+				return;
+			}
+		} catch (error) {
+			console.log('Error submitting form:', error);
+		} finally {
+			setIsSubmitting(false);
+		}
+	};
+
+	return (
+		<div className="wrapper-md upload-page">
+			<h1>Upload a Video</h1>
+
+			{error && <div className="error-field">{error}</div>}
+
+			<form className="rounded-20 shadow-10 gap-6 w-full flex flex-col px-5 py-7.5" onSubmit={handleSubmit}>
+				<FormField
+					id="title"
+					label="Title"
+					value={formData.title}
+					onChange={handleInputChange}
+					placeholder="Enter a clear and concise video title."
+				/>
+
+				<FormField
+					id="description"
+					label="Description"
+					value={formData.description}
+					onChange={handleInputChange}
+					as="textarea"
+					placeholder="Describe what the video is about."
+				/>
+
+				<FileInput
+					id="video"
+					label="Video"
+					accept="video/*"
+					file={video.file}
+					previewUrl={video.previewUrl}
+					inputRef={video.inputRef}
+					onChange={video.handleFileChange}
+					onReset={video.resetFile}
+					type="video"
+				/>
+
+				<FileInput
+					id="thumbnail"
+					label="Thumbnail"
+					accept="image/*"
+					file={thumbnail.file}
+					previewUrl={thumbnail.previewUrl}
+					inputRef={thumbnail.inputRef}
+					onChange={thumbnail.handleFileChange}
+					onReset={thumbnail.resetFile}
+					type="image"
+				/>
+
+				<FormField
+					id="visibility"
+					label="Visibility"
+					value={formData.visibility}
+					onChange={handleInputChange}
+					as="select"
+					options={[
+						{ value: 'public', label: 'Public' },
+						{ value: 'private', label: 'Private' },
+					]}
+				/>
+
+				<button type="submit" disabled={isSubmitting} className="submit-button">
+					{isSubmitting ? 'Uploading...' : 'Upload'}
+				</button>
+			</form>
+		</div>
+	);
+};
+
+export default UploadPage;
