bf64a7e8895a47bccee8d1ff61dce6cda7529f1e
FrankBonanno <fb0023@mix.wvu.edu>
2025-05-21 15:51:06 -0400

    video page

--- app/(root)/video/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-const VideoPage = () => {
-	return (
-		<main className="wrapper page">
-			<h1>Video Details Page</h1>
-		</main>
-	);
-};
-
-export default VideoPage;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,14 @@
 'use client';
 
+import { authClient } from '@/lib/auth-client';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
-const user = {};
-
 const Navbar = () => {
 	const router = useRouter();
+	const { data: session } = authClient.useSession();
+	const user = session?.user;
 
 	return (
 		<header className="navbar">
@@ -19,14 +20,8 @@ const Navbar = () => {
 
 				{user && (
 					<figure>
-						<button onClick={() => router.push('/profile/123456')}>
-							<Image
-								src="/assets/images/dummy.jpg"
-								alt="User Avatar"
-								width={36}
-								height={36}
-								className="rounded-full aspect-square"
-							/>
+						<button onClick={() => router.push(`/profile/${user.id}`)}>
+							<Image src={user.image || ''} alt="user" width={36} height={36} className="rounded-full" />
 						</button>
 						<button className="cursor-pointer">
 							<Image src="/assets/icons/logout.svg" alt="Logout" width={24} height={24} className="rotate-180" />
